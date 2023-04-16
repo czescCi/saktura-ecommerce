a01b5559daf26cae1ea7b1e713559b5619752cd9
Asia <asia.godula@gmail.com>
2023-04-16 12:02:47 +0200

    Work on sanity  hook up

---
+++ saktura-ecommerce/components/Product.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+import { urlFor } from '../lib/client';
+
+function Product({ product: { image, name, slug, price } }) {
+
+  useEffect(() => {
+    console.log('value of slug:', slug);
+  }, [slug]);
+
+  if (!slug) return null;
+
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img src={urlFor(image && image[0])} className='product-image'/>
+        <p className='product-name'>{name}</p>
+        <p className='product-price'>{price} zł</p>
+
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Product
--- saktura-ecommerce/sanity_saktura/schemas/index.ts
+++ saktura-ecommerce/sanity_saktura/schemas/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import banner from './banner';
+import product from './product';
+
+
+export const schemaTypes = [banner, product]
