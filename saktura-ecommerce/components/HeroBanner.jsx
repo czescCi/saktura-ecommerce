7eec19d5ff1836d3ebbfa690b3b6c39e2a09296b
Asia <asia.godula@gmail.com>
2023-05-22 17:10:43 +0200

    Work on hero banner

--- saktura-ecommerce/components/HeroBanner.jsx
+++ saktura-ecommerce/components/HeroBanner.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { urlFor } from '../lib/client';
+import { urlFor } from "../lib/client";
 
 function HeroBanner({ heroBanner }) {
   return (
-    <div className='hero-banner-container'>
-      <div>
+    <div className="hero-banner-container">
+      <div className="images-container">
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt='plant' className='hero-banner-image'/>
+        <img
+          src={urlFor(heroBanner.image && heroBanner.image[0])}
+          alt="plant"
+          className="hero-banner-image"
+        />
+        <img
+          src={urlFor(heroBanner.image && heroBanner.image[1])}
+          alt="plant"
+          className="hero-banner-image"
+        />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
-            <button type='button'>{heroBanner.buttonText}</button>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           {/* <div className='desc'>
             <h5>Description</h5>
@@ -22,7 +31,7 @@ function HeroBanner({ heroBanner }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HeroBanner
+export default HeroBanner;
